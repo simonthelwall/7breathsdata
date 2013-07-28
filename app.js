16c372be8580adc9a332906e38b018e64551a8b5
Neville Dastur <neville@dastur.me.uk>
2013-07-28 13:18:03 +0100

    Start of basic structure and intro

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
+app.use(express.cookieParser('32550073-AE84-4F28-A312-0E3AFC04E162'));
 app.use(express.session());
 app.use(app.router);
 app.use(require('stylus').middleware(__dirname + '/public'));
