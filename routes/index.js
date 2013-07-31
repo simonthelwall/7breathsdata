561382c03e8278cbccb4f7e5b5c7c7e5665f0ad9
Neville Dastur <neville@dastur.me.uk>
2013-07-31 09:39:33 +0100

    Initial site that shows some intro and also registered users number

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@
  * GET home page.
  */
 
+var sbcfg = require('../stackmob.cfg.js');
+var StackMob = require("stackmob-client")({
+	publicKey: sbcfg.stackmob_api_key,
+	secure: true,
+	apiVersion: 1
+});
+
 exports.index = function(req, res){
   res.render('index', { title: '7 Breaths Data' });
 };
--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,9 @@
+var sbcfg = require('../stackmob.cfg.js');
+var StackMob = require("stackmob-client")({
+	publicKey: sbcfg.stackmob_api_key,
+	secure: true,
+	apiVersion: 1
+});
 
 /*
  * GET users listing.
@@ -6,3 +12,19 @@
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+
+exports.count = function(req, res) {
+  var User = StackMob.Model.extend({ schemaName: 'user'});
+  var Users = StackMob.Collection.extend({ model: User });
+  var users = new Users();
+  users.fetch({
+  	success: function(results) {
+      console.log("user:count | Number of users: " + results.length);
+      res.send({count: results.length});
+  	},
+  	error: function() {
+      console.log("user:count | Stackmob fetch error");  		
+      res.send({count: 'Not found, please retry'});
+  	}
+  });
+}
