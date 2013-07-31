3a808957e1d7520543a5cdf448e0a28289f12d19
Neville Dastur <neville@dastur.me.uk>
2013-07-31 11:49:45 +0100

    Show a user count

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,5 @@
+var sys = require('sys');
+
 var sbcfg = require('../stackmob.cfg.js');
 var StackMob = require("stackmob-client")({
 	publicKey: sbcfg.stackmob_api_key,
@@ -19,8 +21,21 @@ exports.count = function(req, res) {
   var users = new Users();
   users.fetch({
   	success: function(results) {
-      console.log("user:count | Number of users: " + results.length);
-      res.send({count: results.length});
+  	  var _users = results.toJSON();
+      console.log("user:count | Number of users: " + _users.length);
+
+      // We can determine the android / ios user split from the device id lenght
+      var _count = _users.length, _android = 0, _ios = 0;
+      for(var i=0; i<_count; i++) {
+      	var result = _users[i];
+      	if (result.device.length === 36 ) {
+      		_ios++;
+      	}
+      	else {
+      		_android++;
+      	}
+      }
+      res.send({count: _count, ios_count: _ios, android_count: _android});
   	},
   	error: function() {
       console.log("user:count | Stackmob fetch error");  		
